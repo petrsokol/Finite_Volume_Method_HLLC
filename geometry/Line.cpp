a837b57dc21a95666a16ad643d0306783d5bc1b4
Petr Sokol <petr.sokol@fs.cvut.cz>
2025-04-14 10:30:10 +0200

    refactor: Added Line.h class to be put in Interface.h class and also in the dual grid

---
+++ geometry/Line.cpp
@@ -0,0 +1,19 @@
+//
+// Created by petrs on 14.04.2025.
+//
+
+#include <cmath>
+#include "Line.h"
+
+
+
+Line::Line (const double ax, const double ay, const double bx, const double by)
+        : len(std::hypot(bx - ax, by - ay)), nx((by - ay) / len), ny((ax - bx) / len)
+{
+}
+
+Line::Line (const Point & a, const Point & b)
+        : Line(a.x, a.y, b.x, b.y)
+{
+}
+
---
+++ geometry/Line.h
@@ -0,0 +1,24 @@
+//
+// Created by petrs on 14.04.2025.
+//
+
+#ifndef GAMM_LINE_H
+#define GAMM_LINE_H
+
+
+#include "Point.h"
+
+class Line
+{
+public:
+  // attributes
+  const double len, nx, ny;
+
+  // constructors
+  Line (const Point & a, const Point & b);
+
+  Line (double ax, double ay, double bx, double by);
+};
+
+
+#endif //GAMM_LINE_H
