e45882631f9aa6c76621e29eb741e8b32ea1d2be
Petr Sokol <petr.sokol@fs.cvut.cz>
2025-04-17 21:06:56 +0200

    feat: overloaded operators for Primitive.h

--- structures/Primitive.h
+++ structures/Primitive.h
@@ -9,16 +9,134 @@
 #include <iostream>
 #include "Conservative.h"
 
-class Primitive {
-
+class Primitive
+{
 public:
-    double rho, rhoU, rhoV, rhoE;
-    double U, u, v, c, p, h;
+  double rho, rhoU, rhoV, rhoE;
+  double U, u, v, c, p, h;
 
-    // Constructors
-    Primitive() = default;
+  // Constructors
+  Primitive () = default;
 
-    Primitive (Conservative w);
+  Primitive (Conservative w);
+
+  // Compound assignment: +=
+  Primitive & operator += (const Primitive & other)
+  {
+    rho += other.rho;
+    rhoU += other.rhoU;
+    rhoV += other.rhoV;
+    rhoE += other.rhoE;
+    U += other.U;
+    u += other.u;
+    v += other.v;
+    c += other.c;
+    p += other.p;
+    h += other.h;
+    return *this;
+  }
+
+  // Compound assignment: -=
+  Primitive & operator -= (const Primitive & other)
+  {
+    rho -= other.rho;
+    rhoU -= other.rhoU;
+    rhoV -= other.rhoV;
+    rhoE -= other.rhoE;
+    U -= other.U;
+    u -= other.u;
+    v -= other.v;
+    c -= other.c;
+    p -= other.p;
+    h -= other.h;
+    return *this;
+  }
+
+  // Compound assignment: *= (scalar)
+  Primitive & operator *= (double scalar)
+  {
+    rho *= scalar;
+    rhoU *= scalar;
+    rhoV *= scalar;
+    rhoE *= scalar;
+    U *= scalar;
+    u *= scalar;
+    v *= scalar;
+    c *= scalar;
+    p *= scalar;
+    h *= scalar;
+    return *this;
+  }
+
+  // Compound assignment: /= (scalar)
+  Primitive & operator /= (double scalar)
+  {
+    rho /= scalar;
+    rhoU /= scalar;
+    rhoV /= scalar;
+    rhoE /= scalar;
+    U /= scalar;
+    u /= scalar;
+    v /= scalar;
+    c /= scalar;
+    p /= scalar;
+    h /= scalar;
+    return *this;
+  }
+
+  // Arithmetic operator: +
+  Primitive operator + (const Primitive & other) const
+  {
+    Primitive result = *this;
+    result += other;
+    return result;
+  }
+
+  // Arithmetic operator: -
+  Primitive operator - (const Primitive & other) const
+  {
+    Primitive result = *this;
+    result -= other;
+    return result;
+  }
+
+  // Arithmetic operator: * (scalar)
+  Primitive operator * (double scalar) const
+  {
+    Primitive result = *this;
+    result *= scalar;
+    return result;
+  }
+
+  // Arithmetic operator: / (scalar)
+  Primitive operator / (double scalar) const
+  {
+    Primitive result = *this;
+    result /= scalar;
+    return result;
+  }
+
+  // Scalar multiplication from left side
+  friend Primitive operator * (double scalar, const Primitive & primitive)
+  {
+    return primitive * scalar;
+  }
+
+  // Assign scalar to all members
+  Primitive & operator = (double scalar)
+  {
+    rho = scalar;
+    rhoU = scalar;
+    rhoV = scalar;
+    rhoE = scalar;
+    U = scalar;
+    u = scalar;
+    v = scalar;
+    c = scalar;
+    p = scalar;
+    h = scalar;
+    return *this;
+  }
 };
 
 
