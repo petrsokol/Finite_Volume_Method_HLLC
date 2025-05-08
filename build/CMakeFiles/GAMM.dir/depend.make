da78c48c8ae24927955da28dd477295ea6632ad0
Petr Sokol <petr.sokol@fs.cvut.cz>
2025-05-08 11:25:34 +0200

    Ignore build and cmake-build-debug directories

--- build/CMakeFiles/GAMM.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GAMM.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/GAMM.dir/geometry/MeshParams.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/GAMM.dir/geometry/MeshParams.cpp.o: \
- /mnt/c/cpp/BP/GAMM/geometry/MeshParams.cpp /usr/include/stdc-predef.h \
- /mnt/c/cpp/BP/GAMM/geometry/MeshParams.h
